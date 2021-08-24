4feb146d66a6ac3e99e63fc4436fec8ccbb401c3
Nechaev S <{ID}+{username}@users.noreply.github.com>
2021-08-24 15:36:35 +0300

    Initial Commit

---
+++ AuthScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AuthScreen
+//
+//  Created by Nechaev Sergey  on 24.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
