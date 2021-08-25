c5a81f96f91eb7d6e8687eae8c97bc61d147eaa1
Nechaev S <{ID}+{username}@users.noreply.github.com>
2021-08-25 15:15:53 +0300

    keyboard logic done

--- AuthScreen/LoginViewController.swift
+++ AuthScreen/LoginViewController.swift
@@ -9,8 +9,8 @@ import UIKit
 
 let users = ["User":"Pass", "User2":"Pass2", "123":"123"]
 
-class LoginViewController: UIViewController {
-
+class LoginViewController: UIViewController, UITextFieldDelegate {
+    
     @IBOutlet weak var userNameAlert: UIButton!
     @IBOutlet weak var passwordAlert: UIButton!
     
@@ -20,7 +20,18 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var logInButton: UIButton!
     
     private let emoji = String(UnicodeScalar(0x2757) ?? "!")
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        userNameTextField.delegate = self
+        userNameTextField.returnKeyType = UIReturnKeyType.next
+        
+        passwordTextField.delegate = self
+        passwordTextField.returnKeyType = UIReturnKeyType.done
+        passwordTextField.enablesReturnKeyAutomatically = true
+    }
+    
     @IBAction func buttonPressed(_ sender: UIButton) {
         switch sender {
         case userNameAlert:
@@ -28,57 +39,66 @@ class LoginViewController: UIViewController {
         case passwordAlert:
             showAlert(with: "Oops\(emoji)", and: "Your password is Pass")
         default:
-            if checkUser(
-                in: users,
-                login: userNameTextField.text ?? "",
-                pass: passwordTextField.text ?? ""
-            ) {
+            if checkUser(in: users, login: userNameTextField.text ?? "", pass: passwordTextField.text ?? "") {
                 self.performSegue(withIdentifier: "login", sender: self)
             } else {
+                passwordTextField.text = ""
                 showAlert(with: "Invalid Login or Password", and: "Please try again")
             }
         }
-        
-        
     }
     
-
-}
-
-
-// MARK: - Private Methods
-extension LoginViewController {
-    
     private func showAlert(with title: String, and massage: String) {
-        let alert = UIAlertController(title: title, message: massage, preferredStyle: .alert)
+        let alert = UIAlertController(
+            title: title,
+            message: massage,
+            preferredStyle: .alert
+        )
         let okAction = UIAlertAction(title: "OK", style: .default)
+        
         alert.addAction(okAction)
         present(alert, animated: true)
     }
-    
+}
+
+
+// MARK: - Auth Methods
+extension LoginViewController {
     private func checkUser(in usersList: [String:String], login: String, pass: String) -> Bool {
         guard let currentUserPass = usersList[login] else { return false }
         return currentUserPass == pass ? true : false
     }
-    
 }
 
-// MARK: - Override methods
+// MARK: - Keyboard methods
 extension LoginViewController {
-    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super .touchesBegan(touches, with: event)
         self.view.endEditing(true)
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        guard let welcomeVC = segue.destination as? WelcomeViewController else {
-            return
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        switch textField {
+        case userNameTextField:
+            passwordTextField.becomeFirstResponder()
+        case passwordTextField:
+            logInButton.sendActions(for: .touchUpInside)
+        default:
+            textField.resignFirstResponder()
         }
+        return true
+    }
+}
+
+// MARK: - Segue methods
+extension LoginViewController {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
         welcomeVC.userNameIs = userNameTextField.text
     }
     
     @IBAction func unwind(for segue: UIStoryboardSegue) {
+        userNameTextField.text = ""
         passwordTextField.text = ""
         dismiss(animated: true)
     }
